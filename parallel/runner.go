a769dedc188716f31af7564455d2b487446992af
Yoav Landman <yoavl@jfrog.com>
2017-01-02 14:29:44 +0200

    Added support for returning errors

--- parallel/runner.go
+++ parallel/runner.go
@@ -1,53 +1,57 @@
-package updown
+package parallel
 
 import (
-        _ "time"
-        "sync"
+	_ "time"
+	"sync"
 )
 
 type Runner struct {
-        parallel chan struct{}
-        tasks    []*task
+	parallel chan struct{}
+	tasks    []*task
 }
 
 type task struct {
-        f   func()
-        err error
+	f func() error
 }
 
 func NewRunner(maxParallel int) *Runner {
-        var parallel chan struct{}
-        if maxParallel > 0 {
-                parallel = make(chan struct{}, maxParallel)
-        }
-        tasks := make([]*task, 0, 50)
-        runner := &Runner{parallel: parallel, tasks: tasks}
-        return runner
+	var parallel chan struct{}
+	if maxParallel > 0 {
+		parallel = make(chan struct{}, maxParallel)
+	}
+	tasks := make([]*task, 0, 50)
+	runner := &Runner{parallel: parallel, tasks: tasks}
+	return runner
 }
 
-func (r *Runner) AddTask(f func()) {
-        task := &task{f:f}
-        r.tasks = append(r.tasks, task)
+func (r *Runner) AddTask(f func() error) {
+	task := &task{f:f}
+	r.tasks = append(r.tasks, task)
 }
 
-func (r *Runner) Run() {
-        limitedParallel := cap(r.parallel) > 0
-        var wg sync.WaitGroup
-        for i, t := range r.tasks {
-                wg.Add(1)
-                if limitedParallel {
-                        r.parallel <- struct{}{}
-                }
-                go func(i int, t *task) {
-                        defer func() {
-                                wg.Done()
-                        }()
-                        t.f()
-                        if limitedParallel {
-                                <-r.parallel
-                        }
-                }(i, t)
+func (r *Runner) Run() []error {
+	limitedParallel := cap(r.parallel) > 0
+	errs := make([]error, len(r.tasks))
+	var wg sync.WaitGroup
+	for i, t := range r.tasks {
+		wg.Add(1)
+		if limitedParallel {
+			r.parallel <- struct{}{}
+		}
+		go func(i int, t *task) {
+			defer func() {
+				wg.Done()
+			}()
 
-        }
-        wg.Wait()
+			if err := t.f(); err != nil {
+				errs[i] = err
+			}
+			if limitedParallel {
+				<-r.parallel
+			}
+		}(i, t)
+
+	}
+	wg.Wait()
+	return errs
 }
--- parallel/runner_test.go
+++ parallel/runner_test.go
@@ -1,38 +1,65 @@
-package updown
+package parallel
 
 import (
-        "testing"
-        "fmt"
-        "math/rand"
-        "time"
+	"testing"
+	"fmt"
+	"math/rand"
+	"time"
+	"math"
+	"errors"
 )
 
 var src = rand.NewSource(time.Now().UnixNano())
 var rnd = rand.New(src)
 
 func TestTask(t *testing.T) {
-        const count = 20
-        results := make(chan int, 100)
+	const count = 70
+	results := make(chan int, 100)
 
-        runner := NewRunner(4)
-        var expectedTotal int
-        for i := 0; i < count; i++ {
-                expectedTotal += i
-                x := i
-                runner.AddTask(func() {
-                        time.Sleep(time.Millisecond * time.Duration(rnd.Intn(50)))
-                        fmt.Printf("%d\n", x)
-                        results <- x
-                })
-        }
-        runner.Run()
+	runner := NewRunner(4)
+	var expectedTotal int
+	var expectedErrorTotal int
+	for i := 0; i < count; i++ {
+		expectedTotal += i
+		if float64(i) > math.Floor(float64(count) / 2) {
+			expectedErrorTotal += i
+		}
 
-        close(results)
-        var resultsTotal int
-        for result := range results {
-                resultsTotal += result
-        }
-        if resultsTotal != expectedTotal {
-                t.Fail()
-        }
+		x := i
+		runner.AddTask(func() error {
+			results <- x
+			time.Sleep(time.Millisecond * time.Duration(rnd.Intn(50)))
+			fmt.Printf("%d\n", x)
+			if float64(x) > math.Floor(float64(count) / 2) {
+				return errors.New(fmt.Sprintf("Second half value %d not counted", x))
+			}
+			return nil
+		})
+	}
+	errors := runner.Run()
+
+	close(results)
+	var resultsTotal int
+	for result := range results {
+		resultsTotal += result
+	}
+	if resultsTotal != expectedTotal {
+		t.Error("Unexpected results total")
+	}
+
+	var errorsTotal int
+	for i, _ := range errors {
+		if errors[i] != nil {
+			errorsTotal += i
+		}
+	}
+	if errorsTotal != expectedErrorTotal {
+		t.Error("Unexpected errors total")
+	}
+	if errorsTotal == 0 {
+		t.Error("Unexpected 0 errors total")
+	}
+
+	fmt.Println("expectedTotal=", expectedTotal)
+	fmt.Println("expectedErrorTotal=", expectedErrorTotal)
 }
