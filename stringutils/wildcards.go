15e312b86c9fcce99d395a55e9338059e4763ce1
Asaf Gabai <77976014+asafgabai@users.noreply.github.com>
2021-11-09 16:06:05 +0200

    Fix replacing regex special chars (#20)
    * On converting wildcard patterns to regex, replacing only a few of the special characters and not all of them - caused a bug in placeholders logic in JFrog CLI.

--- stringutils/wildcards.go
+++ stringutils/wildcards.go
@@ -17,11 +17,20 @@ func MatchWildcardPattern(pattern string, str string) (matched bool, err error) 
 
 // WildcardPatternToRegExp converts a wildcard pattern to a regular expression.
 func WildcardPatternToRegExp(localPath string) string {
-	localPath = regexp.QuoteMeta(localPath)
+	localPath = EscapeSpecialChars(localPath)
 	var wildcard = ".*"
-	localPath = strings.Replace(localPath, "\\*", wildcard, -1)
+	localPath = strings.Replace(localPath, "*", wildcard, -1)
 	if strings.HasSuffix(localPath, "/") || strings.HasSuffix(localPath, "\\") {
 		localPath += wildcard
 	}
 	return "^" + localPath + "$"
+}
+
+func EscapeSpecialChars(path string) string {
+	// We don't replace other special characters (like parenthesis) because they're used in the placeholders logic of the JFrog CLI.
+	var specialChars = []string{".", "^", "$", "+"}
+	for _, char := range specialChars {
+		path = strings.Replace(path, char, "\\"+char, -1)
+	}
+	return path
 }
