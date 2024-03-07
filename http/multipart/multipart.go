65f07f32aac7ae9f8ff95f4ecdaabb241c4b6152
Michael Sverdlov <michaelsv@jfrog.com>
2024-03-07 20:19:30 +0200

    update deps

--- http/multipart/multipart.go
+++ http/multipart/multipart.go
@@ -3,22 +3,20 @@ package multipart
 import (
 	"errors"
 	"fmt"
-	"github.com/cespare/xxhash"
 	ioutils "github.com/jfrog/gofrog/io"
-	"hash"
 	"io"
 	"mime/multipart"
 	"os"
 )
 
-type FileWriterFunc func(fileName string) (io.Writer, error)
+type FileWriterFunc func(fileName string) (io.WriteCloser, error)
 
 func ReadFilesFromStream(multipartReader *multipart.Reader, fileWriterFunc FileWriterFunc) error {
 	for {
 		// Read the next file streamed from client
 		fileReader, err := multipartReader.NextPart()
 		if err != nil {
-			if err == io.EOF {
+			if errors.Is(err, io.EOF) {
 				break
 			}
 			return fmt.Errorf("failed to read file: %w", err)
@@ -32,19 +30,21 @@ func ReadFilesFromStream(multipartReader *multipart.Reader, fileWriterFunc FileW
 		if _, err = io.Copy(fileWriter, fileReader); err != nil {
 			return fmt.Errorf("failed writing '%s' file: %w", fileName, err)
 		}
+		ioutils.Close(fileWriter, &err)
 	}
 	return nil
 }
 
-func WriteFilesToStream(writer io.Writer, filesList []string) error {
-	multipartWriter := multipart.NewWriter(writer)
-	for _, filePath := range filesList {
+func WriteFilesToStream(multipartWriter *multipart.Writer, filePaths []string) (err error) {
+	defer ioutils.Close(multipartWriter, &err)
+
+	for _, filePath := range filePaths {
 		fileReader, err := os.Open(filePath)
 		if err != nil {
 			return fmt.Errorf("failed to open file: %w", err)
 		}
 		defer ioutils.Close(fileReader, &err)
-		fileWriter, err := multipartWriter.CreateFormFile("fieldname", filePath)
+		fileWriter, err := multipartWriter.CreateFormFile("file", filePath)
 		if err != nil {
 			return fmt.Errorf("failed to CreateFormFile: %w", err)
 		}
@@ -54,40 +54,4 @@ func WriteFilesToStream(writer io.Writer, filesList []string) error {
 		}
 	}
 	return nil
-}
-
-func getFileWriter(fileName string) (fileWriter io.Writer, err error) {
-	lockfileMock := map[string]string{
-		"SDFDSFSDFSDFDSF":   "file1",
-		"XXSDFDSFSDFSDFDSF": "file2",
-	}
-	realFileName := lockfileMock[fileName]
-	fileWriter, err = os.Create(realFileName)
-	if err != nil {
-		return nil, fmt.Errorf("create file: %w", err)
-	}
-	// Currently we are using the file name as the hash
-	fileHash := fileName
-	return io.MultiWriter(fileWriter, NewHashWrapper(fileHash)), nil
-}
-
-type HashWrapper struct {
-	hash           hash.Hash64
-	actualChecksum string
-}
-
-func NewHashWrapper(actualChecksum string) *HashWrapper {
-	return &HashWrapper{hash: xxhash.New(), actualChecksum: actualChecksum}
-}
-
-func (hw *HashWrapper) Write(p []byte) (n int, err error) {
-	n, err = hw.hash.Write(p)
-	if fmt.Sprintf("%x", hw.hash.Sum(nil)) != hw.actualChecksum {
-		err = errors.New("checksum mismatch")
-	}
-	return
-}
-
-func myMain(filepath string) {
-	ReadFilesFromStream(nil, getFileWriter)
 }
---
+++ http/multipart/multipart_test.go
@@ -0,0 +1,53 @@
+package multipart
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"mime/multipart"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+)
+
+var targetDir string
+
+func TestReadFilesFromStream(t *testing.T) {
+	sourceDir := t.TempDir()
+
+	// Create 2 file to be transferred via our multipart stream
+	file1 := path.Join(sourceDir, "test1.txt")
+	file2 := path.Join(sourceDir, "test2.txt")
+	file1Content := []byte("test content1")
+	file2Content := []byte("test content2")
+	assert.NoError(t, os.WriteFile(file1, file1Content, 0644))
+	assert.NoError(t, os.WriteFile(file2, file2Content, 0644))
+
+	// Create the multipart writer that will stream our files
+	body := &bytes.Buffer{}
+	multipartWriter := multipart.NewWriter(body)
+	assert.NoError(t, WriteFilesToStream(multipartWriter, []string{file1, file2}))
+
+	// Create local temp dir that will store our files
+	targetDir = t.TempDir()
+
+	// Create the multipart reader that will read the files from the stream
+	multipartReader := multipart.NewReader(body, multipartWriter.Boundary())
+	assert.NoError(t, ReadFilesFromStream(multipartReader, getFileWriter))
+
+	// Validate file 1 transferred successfully
+	content, err := os.ReadFile(filepath.Join(targetDir, "test1.txt"))
+	assert.NoError(t, err)
+	assert.Equal(t, file1Content, content)
+
+	// Validate file 2 transferred successfully
+	content, err = os.ReadFile(filepath.Join(targetDir, "test2.txt"))
+	assert.NoError(t, err)
+	assert.Equal(t, file2Content, content)
+
+}
+
+func getFileWriter(fileName string) (fileWriter io.WriteCloser, err error) {
+	return os.Create(filepath.Join(targetDir, fileName))
+}
